a942f17335972009f937d80eff752a1cacfed478
Mukti Parikh <mukti@onethree.bio>
2024-03-06 14:07:52 -0800

    Added some code to trace and track query completions.

--- scripts/pubmed.py
+++ scripts/pubmed.py
@@ -31,8 +31,8 @@ today_date = datetime.today().date()
 def get_pmids(query: str,
               save_on_server: str,
               search_format: str,
-              starting_index: str,
-              max_records: int,
+              search_starting_index: str,
+              search_max_records: int,
               sorting_criteria:str):
 
     '''
@@ -49,7 +49,7 @@ def get_pmids(query: str,
         search_format = type of output format. XML is default but json is possible.
         starting_index = the starting index of the first paper in the search results. eg. 0
         max_records = max. number of records to obtain.
-                      If nothing is given, it will only return The default is 20 and the max. allowable IDs returned are 10,000.
+                      If nothing is given, it will only return 20. The default is 20 and the max. allowable IDs returned are 10,000.
         sorting_critera = how should results be sorted? Valid values are:
                           pub_date – descending sort by publication date
                           Author – ascending sort by first author
@@ -66,8 +66,8 @@ def get_pmids(query: str,
                      f'&term={query}'
                      f'&usehistory={save_on_server}'
                      f'&retmode={search_format}'
-                     f'&retstart={starting_index}'
-                     f'&retmax={max_records}'
+                     f'&retstart={search_starting_index}'
+                     f'&retmax={search_max_records}'
                      f'&sort={sorting_criteria}')
 
     # Construct url
@@ -90,7 +90,7 @@ def get_pmids(query: str,
     num_ids = len(search_output['esearchresult']['idlist'])
 
     print(f'\tThe actual total number of records matching the search for is {num_result}')
-    print(f'\tThe number of ids retrieved using the esearch utility is {num_ids}')
+    print(f'\tThe number of ids present in the esearch json is {num_ids}')
     print('\tFunction get_pmids complete.')
 
     return search_output
@@ -98,38 +98,34 @@ def get_pmids(query: str,
 def query_and_ids(search_output):
 
     '''
-    Description: Takes the json output and builds a df showing some search metadata.
+    Description: Takes the json output and builds a dictionary showing some search metadata.
     Args:
         search_output = json output from the get_pmids function
     Returns:
-        A df with the following info: the actual query string used for searching, total count matching the query, actual number of results retrieved,
+        A dictionary with the following info: the actual query string used for searching, total count matching the query, actual number of results retrieved,
         and all the PMIDs.
     '''
 
-    # Create a df
-    results_df = pd.DataFrame({'query_string':[],
-                              'pmid':[],
-                              'num_total_matches':[],
-                              'num_retrieved_papers':[]})
+    # Add a string of all total matching PMIDs.
+    id_str = ",".join(search_output['esearchresult']['idlist'])
 
     # Capture metadata about the search results
-    results_df['pmid'] = search_output['esearchresult']['idlist']
-    results_df['query_string'] = search_output['esearchresult']['querytranslation']
-    results_df['num_total_matches'] = int(search_output['esearchresult']['count'])
-    results_df['num_retrieved_papers'] = len(search_output['esearchresult']['idlist'])
     # Add updated date to indicate when these were obtained
-    results_df['acquisition_date'] = today_date
 
-    print('\tMetadata obtained and saved in a df.')
+    metadata_dict = {'query_string': search_output['esearchresult']['querytranslation'],
+                     'num_total_matches': int(search_output['esearchresult']['count']),
+                     'all_matching_pmids':id_str,
+                     'acquisition_date': today_date}
 
-    # Return df
-    return results_df
+    print('\tMetadata obtained and saved in a dictionary.')
+
+    return metadata_dict
 
 def get_abstracts(search_output,
                   content_type: str,
                   ids_from_server: bool,
-                  starting_index: str,
-                  max_records: int):
+                  fetch_starting_index: str,
+                  fetch_max_records: int):
     '''
     Description:
         Takes the json output returned by the esearch utility and uses the efetch utility to return an xml string with abstracts for PMIDs related to a query.
@@ -146,7 +142,7 @@ def get_abstracts(search_output,
         max_records = max. number of records to obtain.
                       This argument necessary only when ids_from_server is True.
     Returns:
-        A list of all PMIDs relevant to the search query.
+        A list of PMIDs relevant to the search query and only those for which content should be extracted (based on fetch_max_records).
         Depending on the content_type (here-abstracts), it will return data for all PMIDs in 1 XML string.
     '''
 
@@ -155,10 +151,10 @@ def get_abstracts(search_output,
         query_key=search_output['esearchresult']['querykey']
         web_env = search_output['esearchresult']['webenv']
 
-        fetchurl_suffix = f'efetch.fcgi?db={database}&query_key={query_key}&WebEnv={web_env}&rettype={content_type}&retstart={starting_index}&retmax={max_records}&retmode=xml'
+        fetchurl_suffix = f'efetch.fcgi?db={database}&query_key={query_key}&WebEnv={web_env}&rettype={content_type}&retstart={fetch_starting_index}&retmax={fetch_max_records}&retmode=xml'
 
-        # Get list of all ids:
-        ids_suffix = f'efetch.fcgi?db={database}&query_key={query_key}&WebEnv={web_env}&rettype=uilist&retmode=text'
+        # Get list of all ids for which data should be collected:
+        ids_suffix = f'efetch.fcgi?db={database}&query_key={query_key}&WebEnv={web_env}&rettype=uilist&retmode=text&retstart={fetch_starting_index}&retmax={fetch_max_records}'
 
         # API call for getting just the PMIDs
         ids_url = base_url + ids_suffix
@@ -168,6 +164,7 @@ def get_abstracts(search_output,
         ids_str = ids_response.text
         # Convert the string to a list of ids
         ids_of_interest = ids_str.strip("\n").split("\n")
+        print(f'\tThe number of matching PMIDs based on the server: {len(ids_of_interest)}')
 
     # Option 2 - If IDs were not saved on the server while using the esearch utility, extract all IDs as a list of string and pass them to the url
 
@@ -222,7 +219,8 @@ def get_data_from_xml(ids_of_interest:list,
 
     print('Iterating through each article and collecting information.')
     # iterate through each paper
-    for child in root.iter('PubmedArticle'):
+    for count,child in enumerate(root.iter('PubmedArticle')):
+        print(f'>>>>>Processing paper {count}')
 
         # from each paper, get the information of interest : title, journal, publication date, abstract, keywords.
         # First create a variable to collect each piece of info.
@@ -311,10 +309,12 @@ def get_data_from_xml(ids_of_interest:list,
 def run_pubmed_pipeline(query,
                         save_on_server,
                         search_format,
-                        starting_index,
-                        max_records,
+                        search_starting_index,
+                        search_max_records,
                         sorting_criteria,
-                        content_type):
+                        content_type,
+                        fetch_starting_index,
+                        fetch_max_records):
 
     print(f'----Running pipeline for the following query:----\n{query}')
 
@@ -322,12 +322,12 @@ def run_pubmed_pipeline(query,
     search_output = get_pmids(query=query,
                               save_on_server=save_on_server,
                               search_format=search_format,
-                              starting_index=starting_index,
-                              max_records=max_records,
+                              search_starting_index=search_starting_index,
+                              search_max_records=search_max_records,
                               sorting_criteria=sorting_criteria)
 
-    print('Collecting metadata about the search results into a df.')
-    metadata_df = query_and_ids(search_output)
+    print('Collecting metadata about the search results into a dictionary.')
+    metadata_dict = query_and_ids(search_output)
 
     print('Using PubMed efetch API to get abstract and other details for relevant PMIDs into an XML string.')
     if save_on_server=='y':
@@ -338,18 +338,23 @@ def run_pubmed_pipeline(query,
     ids_of_interest,pmid_data = get_abstracts(search_output=search_output,
                                               content_type=content_type,
                                               ids_from_server=ids_from_server,
-                                              starting_index=starting_index,
-                                              max_records=max_records)
+                                              fetch_starting_index=fetch_starting_index,
+                                              fetch_max_records=fetch_max_records)
 
     print('Extracting data from XML string and organizing it into a dataframe.')
     papers_df = get_data_from_xml(ids_of_interest,
                                   pmid_data)
 
+    # Collect info about actually how many abstracts were collected
+    num_retrieved_papers = len(set(papers_df['pmid'].values))
+    papers_df['num_abstracts_retrieved'] = num_retrieved_papers
+
+    num_abstracts_requested = len(ids_of_interest)
+    papers_df['num_abstracts_requested'] = num_abstracts_requested
+
     print('Adding in metadata information.')
-    final_df = pd.merge(papers_df,
-                        metadata_df,
-                        on='pmid',
-                        how='inner')
+    for col,val in metadata_dict.items():
+        papers_df[col] = val
 
     print('Pipeline complete, dataframe ready.')
-    return final_df
+    return papers_df
